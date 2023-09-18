22276060d21efce84345d84592bbd8a2b4b2a09e
yanki <yankikis97@gmail.com>
2023-09-18 23:22:16 +0300

    create Java101

---
+++ src/Ortalama/NotOrtalamasi.java
@@ -0,0 +1,39 @@
+package Ortalama;
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        int math, phy, chem, turkish, history, music;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Matematik notunu giriniz: ");
+        math = input.nextInt();
+
+        System.out.print("Fizik notunu giriniz: ");
+        phy = input.nextInt();
+
+        System.out.print("Kimya notunu giriniz: ");
+        chem = input.nextInt();
+
+        System.out.print("Türkçe notunu giriniz: ");
+        turkish = input.nextInt();
+
+        System.out.print("Tarih notunu giriniz: ");
+        history = input.nextInt();
+
+        System.out.print("Müzik notunu giriniz: ");
+        music = input.nextInt();
+
+        int sum;
+        sum = (math + phy + chem + turkish + history + music);
+
+        double result;
+        result = sum / 6.0;
+
+        System.out.println("Ortalama: " + result);
+        System.out.println(result < 60 ? "Sınıfta kaldı." : "Sınıfı Geçti!");
+
+
+
+    }
+}
