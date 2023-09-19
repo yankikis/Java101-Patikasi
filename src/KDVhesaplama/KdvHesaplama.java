e8eb7589aa9f9fc4e3330e58a2cd512b11b407c5
yanki <yankikis97@gmail.com>
2023-09-19 09:20:03 +0300

    create kdvHesaplama

---
+++ src/KDVhesaplama/KdvHesaplama.java
@@ -0,0 +1,34 @@
+package KDVhesaplama;
+import java.util.Scanner;
+
+public class KdvHesaplama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Para değerini giriniz: ");
+        double para = input.nextDouble();
+        double kdv, sonuc;
+
+        if (para > 0 && para < 1000){
+
+            kdv = ((para * 18) / 100);
+            sonuc = kdv + para;
+
+            System.out.println("KDV'siz fiyat: "+ para);
+            System.out.println("KDV değeri: "+ kdv);
+            System.out.println("KDV'li fiyat: "+ sonuc);
+
+        }
+        else if (para >= 1000){
+
+            kdv = ((para * 8)/100);
+            sonuc = kdv + para;
+
+            System.out.println("KDV'siz fiyat: "+ para);
+            System.out.println("KDV değeri: "+ kdv);
+            System.out.println("KDV'li fiyat: "+ sonuc);
+
+        }
+        else System.out.println("Error!");
+    }
+}
