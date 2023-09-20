065f8a804923eafd8e54bc26deb81184de2040d4
yanki <yankikis97@gmail.com>
2023-09-21 00:12:32 +0300

    create EtkinlikOnerme

--- src/KosulluIfadelerVeKodBloklari/EtkinlikOnerme.java
+++ src/KosulluIfadelerVeKodBloklari/EtkinlikOnerme.java
@@ -1,9 +1,24 @@
 package KosulluIfadelerVeKodBloklari;
 import java.util.Scanner;
+
 public class EtkinlikOnerme {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int heat;
 
+        System.out.print("Hava sıcaklığını giriniz: ");
+        heat = input.nextInt();
 
+        if (heat > 25) {
+            System.out.println("Yüzmeye gidebilirsiniz.");
+        }
+        else if (heat > 15){
+            System.out.println("Pikniğe gidebilirsiniz.");
+        }
+        else if (heat > 5){
+            System.out.println("Sinemaya gidebilirsiniz.");
+        }
+        else
+            System.out.println("Kayak yapabilirsiniz.");
     }
 }
