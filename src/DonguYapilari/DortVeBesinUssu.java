a54fb9c87958993255e20563747dad5aa2a4735c
yanki <yankikis97@gmail.com>
2023-09-24 14:07:07 +0300

    change DortVeBesinUssu

--- src/DonguYapilari/DortVeBesinUssu.java
+++ src/DonguYapilari/DortVeBesinUssu.java
@@ -1,6 +1,6 @@
 package DonguYapilari;
 import java.util.Scanner;
-public class UsBulanProgram {
+public class DortVeBesinUssu {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n;
