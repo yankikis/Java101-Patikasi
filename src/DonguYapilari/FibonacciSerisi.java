695d1fb13f9f51098826d78f42b44f6e16f3daf0
yanki <yankikis97@gmail.com>
2023-09-25 15:25:22 +0300

    create FibonacciSerisi

---
+++ src/DonguYapilari/FibonacciSerisi.java
@@ -0,0 +1,18 @@
+package DonguYapilari;
+import java.util.Scanner;
+public class FibonacciSerisi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n, first = 0, second = 1, next;
+        System.out.println("Bir sayı giriniz: ");
+        n = input.nextInt();
+
+        System.out.print(n + " elemanlı Fibonacci serisi: " +first+ " "+second+ " " );
+        for (int i = 0; i <= n-2; i++){
+            next = first + second;
+            first = second;
+            second = next;
+            System.out.print(next + " ");
+        }
+    }
+}
